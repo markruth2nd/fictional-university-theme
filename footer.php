7e77a9e83e857bf6eb1688093b1ee98e6c6da474
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-10-02 19:00:09 +0100

    created WP menu's and linked them to header & footer.php(They are now commented out).. Also in header.php, added class to current menu item with if statement and linked children to menu selection with wp_get_post_parent_id

--- footer.php
+++ footer.php
@@ -24,6 +24,11 @@
             <div class="site-footer__col-three">
             <h3 class="headline headline--small">Learn</h3>
             <nav class="nav-list">
+                <!-- <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'footerLocationOne'
+                ));
+                ?> -->
                 <ul>
                 <li><a href="#">Legal</a></li>
                 <li><a href="<?php echo site_url('/privacy-policy') ?>">Privacy</a></li>
@@ -36,6 +41,11 @@
         <div class="site-footer__col-four">
             <h3 class="headline headline--small">Connect With Us</h3>
             <nav>
+                <!-- <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'footerLocationTwo'
+                ));
+                ?> -->
             <ul class="min-list social-icons-list group">
                 <li>
                 <a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
