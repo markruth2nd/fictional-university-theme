1e6dacdcf8e60eb97d2567f6df5aa853cbc378df
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-10-11 19:49:28 +0100

    addded the page banner function to all pages including all archive,single & page PHP files. Take Back, not all required the array part of the function such as the single PHP files

--- archive.php
+++ archive.php
@@ -1,20 +1,11 @@
 <?php
 
 get_header();
+pageBanner(array(
+    'title' => get_the_archive_title(),
+    'subtitle' => get_the_archive_description()
+));
 ?>
-
-<div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
-        <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title"><?php the_archive_title(); ?></h1>
-        
-    <!-- The below is a breakdown of the_archive_title(); function if need to do seperately -->
-<!-- <?php if (is_category()) { single_cat_title(); } if (is_author()) { echo 'Posts by '; the_author();} ?>-->
-    <div class="page-banner__intro">
-        <p><?php the_archive_description(); ?></p>
-        </div>
-    </div>
-</div>
 
 <div class="container container--narrow page-section">
     <?php 
