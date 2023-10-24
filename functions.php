18f344fc1e3eaebe5be5cc678ffb98092d249cf5
Mark Baldry <markbaldry@harrisoncarloss.com>
2023-10-24 10:51:54 +0100

    removed API Key

--- functions.php
+++ functions.php
@@ -80,7 +80,7 @@ function university_adjust_queries($query) {
 add_action('pre_get_posts', 'university_adjust_queries');
 
 function universityMapKey($api) {
-    $api['key'] = 'AIzaSyCeSa4OEpOmz_YhGZln5ZXO4TpFk225IwA';
+    $api['key'] = '';
     return $api;
 }
 
