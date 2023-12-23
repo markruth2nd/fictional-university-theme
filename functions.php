39650b9dc846776b9cd59ece830bf5259a07db12
Mark Baldry <markbaldry@harrisoncarloss.com>
2023-12-23 14:41:39 +0000

    ensured user notes remain private, already added some previosly in functions.php

--- functions.php
+++ functions.php
@@ -155,6 +155,27 @@ function ourLoginTitle() {
     return get_bloginfo('name');
 }
 
+
+// Force note posts to be private
+add_filter('wp_insert_post_data', 'makeNotePrivate', 10, 2);
+
+function makeNotePrivate($data, $postarr) {
+    if ($data['post_type'] == 'note') {
+        if (count_user_posts(get_current_user_id(), 'note') > 4 AND !$postarr['ID']) {
+            die("You have reached your note limit.");
+        }
+
+        $data['post_content'] = sanitize_textarea_field($data['post_content']);
+        $data['post_title'] = sanitize_text_field($data['post_title']);
+    }
+
+    if ($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
+        $data['post_status'] = "private";
+    }
+
+    return $data;
+}
+
 /* CREATED NEW FILE CALLED 'university-post-types.php' 'mu-plugins' FOLDER IN 'wp-content' folder with the below function which adds a new custom post type called events, below is the code which is in this new file
 
 <?php 
