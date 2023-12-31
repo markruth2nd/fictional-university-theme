35a68400c9e8f269bcea5ce097e5e7c7b4b5bbc9
Mark Baldry <markbaldry@harrisoncarloss.com>
2023-12-31 13:59:58 +0000

    Completed challenge to make homepage slideshow dynamic, completing this via custom post type was the successful way as I struggled getting the repeater working correctly

--- functions.php
+++ functions.php
@@ -45,12 +45,20 @@ function pageBanner($args = NULL) {
 
 function university_files() {
     wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyCeSa4OEpOmz_YhGZln5ZXO4TpFk225IwA',NULL, '1.0', true);
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
-    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
-    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-
+    
+    if (strstr($_SERVER['SERVER_NAME'], 'fictional-university.local')) {
+        wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+    } else {
+        wp_enqueue_script('our-vendors-js', get_theme_file_uri('/build/vendors-index.js'), NULL, '1.0', true);
+        wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+        wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+        wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+    }
+    
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+    
     wp_localize_script( 'main-university-js', 'universityData' , array(
         'root_url' => get_site_url(),
         'nonce' => wp_create_nonce('wp_rest')
@@ -177,6 +185,14 @@ function makeNotePrivate($data, $postarr) {
 
     return $data;
 }
+
+
+add_filter( 'ai1wm_exclude_content_from_export', function($exclude_filters) {
+    $exclude_filters[] = 'themes/fictional-university-theme/node_modules';
+    return $exclude_filters;
+});
+
+
 
 /* CREATED NEW FILE CALLED 'university-post-types.php' 'mu-plugins' FOLDER IN 'wp-content' folder with the below function which adds a new custom post type called events, below is the code which is in this new file
 
